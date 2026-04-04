85e773e9aa9ca5e71c54458e96524fe3d45f0529
ThanhThankC <156212398+ThanhThankC@users.noreply.github.com>
2026-04-04 18:58:26 +0700

    add gif

--- Dang/gifs.js
+++ Dang/gifs.js
@@ -1,19 +1,15 @@
 window.WAITING_GIFS = [
-  // Quốc kỳ Việt Nam bay
-  "https://media.tenor.com/Fp2rY2gLbpEAAAAC/vietnam-ho-chi-minh.gif",
+  // Duyệt binh
+  "https://media1.tenor.com/m/ofyem1Uk49YAAAAC/vietnam-march-victory-day.gif",
   // Cờ đỏ sao vàng
-  "https://media.tenor.com/iHLlNI1XNRQAAAAC/h%E1%BB%99i-lhpnt%E1%BB%89nh-c%C3%A0mau.gif",
+  "https://media1.tenor.com/m/HlpS6ZPkW0cAAAAd/h%E1%BB%99i-lhpnt%E1%BB%89nh-c%C3%A0mau.gif",
   // Cờ Đảng + cờ đỏ
-  "https://media.tenor.com/9bQ3j9bS1ZEAAAAC/vietnam-flag.gif",
+  "https://media1.tenor.com/m/uR3_f6vNZw0AAAAC/h%E1%BB%99i-lhpnt%E1%BB%89nh-c%C3%A0mau.gif",
   // Bác Hồ + cờ
-  "https://media.tenor.com/Jqj2RzB8v_sAAAAC/vietnam.gif",
-  // Lễ kết nạp Đảng
-  "https://media.tenor.com/QrUqDNfGv2MAAAAC/party-flag.gif",
+  "https://media1.tenor.com/m/lJIT2uBWWSkAAAAd/vietnam-ho-chi-minh.gif",
 ];
  
-/**
- * Trả về một GIF URL ngẫu nhiên từ danh sách
- */
+
 window.getRandomWaitingGif = function() {
   const list = window.WAITING_GIFS;
   return list[Math.floor(Math.random() * list.length)];
